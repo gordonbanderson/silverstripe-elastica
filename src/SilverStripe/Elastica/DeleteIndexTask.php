1fb2c15b9e0b1f8a6b4dccdf957a7f7deb6bd073
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-07-27 17:12:27 +0700

    MINOR: Change name of task to represent the fact that a deletion of the index is happening

--- src/SilverStripe/Elastica/DeleteIndexTask.php
+++ src/SilverStripe/Elastica/DeleteIndexTask.php
@@ -5,11 +5,11 @@ namespace SilverStripe\Elastica;
 /**
  * Defines and refreshes the elastic search index.
  */
-class ResetIndexTask extends \BuildTask {
+class DeleteIndexTask extends \BuildTask {
 
-	protected $title = 'Elastic Search Reset Index';
+	protected $title = 'Elastic Search Index Deletion';
 
-	protected $description = 'Resets/clears the configured elastic search index';
+	protected $description = 'Deletes the configured elastic search index';
 
 	/**
 	 * @var ElasticaService
@@ -20,16 +20,17 @@ class ResetIndexTask extends \BuildTask {
 		$this->service = $service;
 	}
 
+	/**
+	 * Execute the task to delete the currently configured index
+	 */
 	public function run($request) {
 		$message = function ($content) {
 			print(\Director::is_cli() ? "$content\n" : "<p>$content</p>");
 		};
 
-		$message('Defining the mappings');
 		$this->service->define();
-
-		$message('Resetting the index');
 		$this->service->reset();
+		$message('Successfully deleted configured index');
 	}
 
 }
