b93cd3943e86ee67a82c5edddecc5a01af5e11fc
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-08-10 23:18:51 +0700

    FIX: Add method for aggregation

--- src/SilverStripe/Elastica/ElasticaQueryAggregationManipulator.php
+++ src/SilverStripe/Elastica/ElasticaQueryAggregationManipulator.php
@@ -1,10 +1,18 @@
 <?php
 
-interface ElasticaQueryManipulator {
+interface ElasticaQueryAggregationManipulator {
 	/**
 	 * Alter the query or add to it, perhaps for example adding aggregation
 	 * @param  Elastic\Query &$query query object from Elastica
 	 * @return [type]         [description]
 	 */
 	public function augmentQuery(&$query);
+
+	/**
+	 * Manipulate the array of aggregations prior to rendering them
+	 * in a template.
+	 * @param  [type] &$aggs [description]
+	 * @return [type]        [description]
+	 */
+	public function manipulateAggregation(&$aggs);
 }
