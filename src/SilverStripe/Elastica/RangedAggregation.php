d1e5801fa36e4b17c7b34a7a9c9395f03dbe6b27
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-08-15 17:47:25 +0700

    WIP: Choosing of ranged facet now appears to be working

--- src/SilverStripe/Elastica/RangedAggregation.php
+++ src/SilverStripe/Elastica/RangedAggregation.php
@@ -4,7 +4,6 @@ class RangedAggregation {
 
 	private static $ranged_aggregations = array();
 
-	private $range_filters = array();
 
 	public function __construct($title, $field) {
 		$this->Title = $title;
@@ -13,24 +12,63 @@ class RangedAggregation {
 		self::$ranged_aggregations[$title] = $this;;
 	}
 
+
 	public function addRange($from, $to, $name) {
 		$this->Range->addRange($from,$to,$name);
 	}
+
 
 	public function getRangeAgg() {
 		return $this->Range;
 	}
 
+/*
+[range] => Array
+        (
+            [field] => AspectRatio
+            [ranges] => Array
+                (
+                    [0] => Array
+                        (
+                            [from] => 0
+                            [to] => 0.3
+                            [key] => Panoramic
+                        )
+
+                    [1] => Array
+                        (
+                            [from] => 0.3
+                            [to] => 0.9
+                            [key] => Horizontal
+ */
+
 	public function getFilter($chosenName) {
-		$range = array('gt' => 0, 'lte' => 0.3);
-        $filter = new Elastica\Filter\Range('AspectRatio', $range);
-        return $filter;
+
+		$rangeArray = $this->Range->toArray()['range']['ranges'];
+
+		foreach ($rangeArray as $range) {
+			if ($range['key'] === $chosenName) {
+				$from = null;
+				$to = null;
+				if (isset($range['from'])) {
+					$from = $range['from'];
+				}
+				if (isset($range['to'])) {
+					$to = $range['to'];
+				}
+				$rangeFilter = array('gte' => (string)$from, 'lt' => (string)$to);
+		        $filter = new Elastica\Filter\Range('AspectRatio', $rangeFilter);
+		        return $filter;
+			}
+		}
+
 	}
 
 
 	public static function getByTitle($title) {
 		return self::$ranged_aggregations[$title];
 	}
+
 
 	public static function getTitles() {
 		return array_keys(self::$ranged_aggregations);
