feee1e7e90f5a79e928eacb10c22740c49f16357
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-11 22:42:10 +0700

    ENHANCEMENT: German indexing now working

--- src/SilverStripe/Elastica/indexing/GermanIndexSettings.php
+++ src/SilverStripe/Elastica/indexing/GermanIndexSettings.php
@@ -5,26 +5,10 @@
 */
 class GermanIndexSettings  extends AbstractIndexSettings {
 
-	function getIndexSettings() {
-		/*
-		See the following URLS:
-		- https://www.elastic.co/guide/en/elasticsearch/guide/current/configuring-language-analyzers.html
-
-		we want stemmed, non stemmed
-		 */
-		$json = '{
-		  "settings": {
-		    "analysis": {
-		      "analyzer": {
-		        "my_english": {
-		          "type": "german",
-		          "stopwords": "_german_"
-		        }
-		      }
-		    }
-		  }
-		}';
-
-		return json_decode($json);
+	public function __construct() {
+		$this->setStopWords('_german_');
+		$this->setAsciiFolding(true);
+		$this->setAnalyzerType('german');
 	}
+
 }
