d80a86079c9743bc6625f9052ac1d1b3885feafd
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-11 22:42:31 +0700

    ENHANCEMENT: Thai indexing working

--- src/SilverStripe/Elastica/indexing/ThaiIndexSettings.php
+++ src/SilverStripe/Elastica/indexing/ThaiIndexSettings.php
@@ -3,34 +3,11 @@
 /**
 *
 */
-class ThaiIndexSettings {
+class ThaiIndexSettings extends AbstractIndexSettings {
 
-	function getIndexSettings() {
-		/*
-		See the following URLS:
-		- https://www.elastic.co/guide/en/elasticsearch/guide/current/configuring-language-analyzers.html
-
-		we want stemmed, non stemmed
-		 */
-		$json = '{
-		  "settings": {
-		    "analysis": {
-		      "analyzer": {
-		        "my_english": {
-		          "type": "english",
-		          "stem_exclusion": [ "organization", "organizations" ],
-		          "stopwords": [
-		            "a", "an", "and", "are", "as", "at", "be", "but", "by", "for",
-		            "if", "in", "into", "is", "it", "of", "on", "or", "such", "that",
-		            "the", "their", "then", "there", "these", "they", "this", "to",
-		            "was", "will", "with"
-		          ]
-		        }
-		      }
-		    }
-		  }
-		}';
-
-		return json_decode($json);
+	public function __construct() {
+		$this->setStopWords('_thai_');
+		$this->setAsciiFolding(true);
+		$this->setAnalyzerType('thai');
 	}
 }
