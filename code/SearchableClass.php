5d95aa5cc24738522f5ae58e88f289d47501cea8
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-13 10:38:20 +0700

    ENHANCEMENT: Model for searchable class

---
+++ code/SearchableClass.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+* Represents a searchable class in Elastica for editing purposes
+*/
+class SearchableClass extends DataObject {
+
+	private static $db = array('Name' => 'Varchar');
+
+	private static $has_many = array('SearchableFields' => 'SearchableField');
+
+	private static $display_fields = array('Name');
+
+	function getCMSFields() {
+
+		$fields = new FieldList();
+
+		$fields->push( new TabSet( "Root", $mainTab = new Tab( "Main" ) ) );
+		$mainTab->setTitle( _t( 'SiteTree.TABMAIN', "Main" ) );
+
+		$fields->addFieldToTab( 'Root.Main',  $nf = new TextField( 'Name', 'Name') );
+		$nf->setReadOnly(true);
+		$nf->setDisabled(true);
+
+		$config = GridFieldConfig_RelationEditor::create();
+		$config->getComponentByType('GridFieldDataColumns')->setDisplayFields(array(
+            'Name' => 'Name',
+            'Weight' => 'Weight',
+            'HumanReadableIsSearched' => 'Search this field?'
+        ));
+
+
+        $gridField = new GridField(
+            'SearchableField', // Field name
+            'Field Name', // Field title
+            SearchableField::get()->filter('SearchableClassID', $this->ID)->sort('Name'),
+            $config
+        );
+
+        $fields->addFieldToTab('Root.Main', $gridField);
+
+		/*
+	    $fields = new FieldList();
+	    $fields->addFieldToTab('Root.Main', new TextField('Name', 'Name'));
+
+
+        */
+	    return $fields;
+	}
+
+
+	/*
+		$searchTabName = 'Root.'._t('SiteConfig.ELASTICA', 'Search');
+		$fields->addFieldToTab($searchTabName, $h1=new LiteralField('SearchInfo',
+			_t('SiteConfig.ELASTICA_SEARCH_INFO', "Select a class to edit the search fields of that class")));
+
+		$config = GridFieldConfig_RelationEditor::create();
+		$config->getComponentByType('GridFieldDataColumns')->setDisplayFields(array(
+            'Name' => 'Name'
+        ));
+
+        $gridField = new GridField(
+            'SearchableClass', // Field name
+            'Class Name', // Field title
+            SearchableClass::get()->sort('Name'), // List of all related students
+            $config
+        );
+
+        $fields->addFieldToTab($searchTabName, $gridField);
+	 */
+}
