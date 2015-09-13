7a2dde71c25aafcdf6b4c32cd4bbf4f4917fd71d
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-13 16:20:13 +0700

    ENHANCEMENT: Added in SiteTree flag

--- code/SearchableClass.php
+++ code/SearchableClass.php
@@ -5,11 +5,11 @@
 */
 class SearchableClass extends DataObject {
 
-	private static $db = array('Name' => 'Varchar');
+	private static $db = array('Name' => 'Varchar', 'IsSiteTree' => 'Boolean');
 
 	private static $has_many = array('SearchableFields' => 'SearchableField');
 
-	private static $display_fields = array('Name');
+	private static $display_fields = array('Name', 'IsSiteTreeHumanReadable');
 
 	function getCMSFields() {
 
@@ -54,23 +54,7 @@ class SearchableClass extends DataObject {
 	}
 
 
-	/*
-		$searchTabName = 'Root.'._t('SiteConfig.ELASTICA', 'Search');
-		$fields->addFieldToTab($searchTabName, $h1=new LiteralField('SearchInfo',
-			_t('SiteConfig.ELASTICA_SEARCH_INFO', "Select a class to edit the search fields of that class")));
-
-		$config = GridFieldConfig_RelationEditor::create();
-		$config->getComponentByType('GridFieldDataColumns')->setDisplayFields(array(
-            'Name' => 'Name'
-        ));
-
-        $gridField = new GridField(
-            'SearchableClass', // Field name
-            'Class Name', // Field title
-            SearchableClass::get()->sort('Name'), // List of all related students
-            $config
-        );
-
-        $fields->addFieldToTab($searchTabName, $gridField);
-	 */
+	public function IsSiteTreeHumanReadable() {
+		return $this->IsSiteTree ? 'Yes' : 'No';
+	}
 }
