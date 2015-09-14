f010253fcbec8037c95fad6bee24452cf8f42516
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-14 14:04:21 +0700

    ENHANCEMENT: Editing of searchable fields for a search page

--- javascript/elasticaedit.js
+++ javascript/elasticaedit.js
@@ -3,6 +3,15 @@ console.log('el edit');
 (function($) {
 	$(document).ready(function() {
 
+		var node = $('#Form_EditForm_SiteTreeOnly');
+		var originalSiteTreeOnly = node.is(':checked');
+		var originalClassesToSearch = $('#Form_EditForm_ClassesToSearch').val();
+				console.log(originalClassesToSearch);
+
+
+		node.data('SiteTree', originalSiteTreeOnly);
+		node.data('Classes', originalClassesToSearch);
+
 		$('#Form_EditForm_SiteTreeOnly').entwine({
 			onchange: function(e) {
 				enableOrDisableSiteTreeList();
@@ -15,10 +24,51 @@ console.log('el edit');
 			}
 		});
 
+		$('#Form_EditForm_SiteTreeOnly').entwine({
+			onchange: function(e) {
+				showOrHideSearchFields();
+			}
+		});
+
+		$('#Form_EditForm_ClassesToSearch').entwine({
+			oninput: function(e) {
+				showOrHideSearchFields();
+			}
+		});
+
 		// this is run first time only, to prime the form
 		enableOrDisableSiteTreeList();
 
 	});
+
+
+	function showOrHideSearchFields() {
+		var searchFieldsPanel = $('#Form_EditForm_ElasticSearchPageSearchField');
+		var searchFieldsIntro = $('#SearchFieldIntro');
+		var searchDetailsMessage = $('#SearchFieldsMessage');
+
+		var node = $('#Form_EditForm_SiteTreeOnly');
+		var originalSiteTreeOnly = node.data('SiteTree');
+		var originalClassesToSearch = node.data('Classes');
+		var currentSiteTreeOnly = $('#Form_EditForm_SiteTreeOnly').is(':checked');
+		var currentClassesToSearch = $('#Form_EditForm_ClassesToSearch').val();
+
+		console.log(originalClassesToSearch,currentClassesToSearch);
+
+		if (
+			(currentSiteTreeOnly === originalSiteTreeOnly) &&
+			(currentClassesToSearch === originalClassesToSearch)
+		) {
+			searchFieldsPanel.removeClass('hide');
+			searchFieldsIntro.removeClass('hide');
+			searchDetailsMessage.attr('style','display: none;');
+		} else {
+			searchFieldsPanel.addClass('hide');
+			searchFieldsIntro.addClass('hide');
+			searchDetailsMessage.attr('style','display: block;');
+		}
+	}
+
 
 	/* Hide the classes to search list when Site Tree Only is selected */
 	function enableOrDisableSiteTreeList() {
