34a3daafe6b909a1e942df01c9d5ca436b544454
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-16 22:41:26 +0700

    ENHANCEMENT: Extension to clean the elastica cache every build

---
+++ src/SilverStripe/Elastica/ClearElasticFieldCacheExtension.php
@@ -0,0 +1,9 @@
+<?php
+namespace SilverStripe\Elastica;
+
+class ClearElasticFieldCacheExtension extends \Extension {
+	public function onAfterInit() {
+		$cache = \SS_Cache::factory('elasticsearch');
+		$cache->clean();
+	}
+}
