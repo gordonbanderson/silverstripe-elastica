9c7e92e5972146a27a1b7031776ad3d4fe631ebb
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-18 17:02:26 +0700

    WIP: Searchable test work in progress

---
+++ tests/SearchableTest.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Teste the functionality of the Searchable extension
+ * @package elastica
+ */
+class SearchableTest extends SapphireTest {
+	public static $fixture_file = 'elastica/tests/ElasticaTest.yml';
+
+	protected $extraDataObjects = array(
+		'SearchableTestPage','FlickrPhoto','FlickrAuthor','FlickrSet','FlickrTag'
+	);
+
+	public function setUp() {
+		// this needs to be called in order to create the list of searchable
+		// classes and fields that are available.  Simulates part of a build
+		$classes = array('SearchableTestPage','SiteTree','Page','FlickrPhoto','FlickrSet',
+			'FlickrTag', 'FlickrAuthor', 'FlickrSet');
+		$this->requireDefaultRecordsFrom = $classes;
+
+		// add Searchable extension where appropriate
+		FlickrSet::add_extension('SilverStripe\Elastica\Searchable');
+		FlickrPhoto::add_extension('SilverStripe\Elastica\Searchable');
+		FlickrTag::add_extension('SilverStripe\Elastica\Searchable');
+		FlickrAuthor::add_extension('SilverStripe\Elastica\Searchable');
+		SearchableTestPage::add_extension('SilverStripe\Elastica\Searchable');
+
+		// load fixtures
+		parent::setUp();
+	}
+
+	/**
+	 * Test a valid identifier
+	 */
+	public function testMapping() {
+		$flickrPhoto = $this->objFromFixture('FlickrPhoto', 'photo0001');
+		$mapping = $flickrPhoto->getElasticaMapping();
+
+		//array of mapping properties
+		$properties = $mapping->getProperties();
+
+		// check strings
+		$shouldBeString = array('Title','Description');
+		$shouldBeInt = array('ISO','FlickrID','FocalLength35mm');
+		$shouldBeBoolean = array('IsSiteTree');
+		$shouldBeDouble = array('Aperture');
+		$shouldBeDateTime = array('TakenAt');
+		$shouldBeDate = array('FirstViewed');
+
+		// check strings
+		$expectedStandardArray = array('type' => 'string', 'analyzer' => 'standard');
+		foreach ($shouldBeString as $fieldName) {
+			$fieldProperties = $properties[$fieldName];
+
+			$type = $fieldProperties['type'];
+			$analyzer = $fieldProperties['analyzer'];
+			$this->assertEquals('string', $type);
+
+			// check for stemmed analysis
+			$this->assertEquals('stemmed', $analyzer);
+
+			// check for unstemmed analaysis
+			$this->assertEquals($expectedStandardArray,$fieldProperties['fields']['standard']);
+
+			// check for only 3 entries
+			$this->assertEquals(3, sizeof(array_keys($fieldProperties)));
+		}
+
+		// check ints
+		foreach ($shouldBeInt as $fieldName) {
+			$fieldProperties = $properties[$fieldName];
+			$type = $fieldProperties['type'];
+			$this->assertEquals(1, sizeof(array_keys($fieldProperties)));
+			$this->assertEquals('integer',$type);
+		}
+
+
+		// check doubles
+		foreach ($shouldBeDouble as $fieldName) {
+			$fieldProperties = $properties[$fieldName];
+			$type = $fieldProperties['type'];
+			$this->assertEquals(1, sizeof(array_keys($fieldProperties)));
+			$this->assertEquals('double',$type);
+		}
+
+		// check boolean
+		foreach ($shouldBeBoolean as $fieldName) {
+			$fieldProperties = $properties[$fieldName];
+			$type = $fieldProperties['type'];
+			$this->assertEquals(1, sizeof(array_keys($fieldProperties)));
+			$this->assertEquals('boolean',$type);
+		}
+
+
+		foreach ($shouldBeDate as $fieldName) {
+			$fieldProperties = $properties[$fieldName];
+			$type = $fieldProperties['type'];
+			$this->assertEquals(2, sizeof(array_keys($fieldProperties)));
+			$this->assertEquals('date',$type);
+			$this->assertEquals('y-M-d', $fieldProperties['format']);
+		}
+
+
+
+		// check date time, stored in Elasticsearch as a date with a different format than above
+		foreach ($shouldBeDateTime as $fieldName) {
+			$fieldProperties = $properties[$fieldName];
+			$type = $fieldProperties['type'];
+			$this->assertEquals(2, sizeof(array_keys($fieldProperties)));
+			$this->assertEquals('date',$type);
+			$this->assertEquals('y-M-d H:m:s', $fieldProperties['format']);
+		}
+	}
+
+
+	public function testGetType() {
+		//A type in Elasticsearch is used to represent each SilverStripe content type,
+		//the name used being the Silverstripe $fieldName
+
+		$flickrPhoto = $this->objFromFixture('FlickrPhoto', 'photo0001');
+		$type = $flickrPhoto->getElasticaType();
+		$this->assertEquals('FlickrPhoto', $type);
+	}
+
+
+}
