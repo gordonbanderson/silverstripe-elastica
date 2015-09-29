9e1b78d41c4803b29316a034d400b666be7e551d
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-29 12:22:47 +0700

    WIP: Weeding out search related bugs

--- tests/SearchAndIndexingTest.php
+++ tests/SearchAndIndexingTest.php
@@ -4,60 +4,126 @@
  * Teste the functionality of the Searchable extension
  * @package elastica
  */
-class SearchAndIndexingTest extends SapphireTest {
+class SearchAndIndexingTest extends ElasticsearchBaseTest {
 	//public static $fixture_file = 'elastica/tests/lotsOfPhotos.yml';
 	public static $fixture_file = 'elastica/tests/lotsOfPhotos.yml';
 
-	protected $extraDataObjects = array(
-		'SearchableTestPage','FlickrPhoto','FlickrAuthor','FlickrSet','FlickrTag'
-	);
 
-	public function setUp() {
-		// this needs to be called in order to create the list of searchable
-		// classes and fields that are available.  Simulates part of a build
-		$classes = array('SearchableTestPage','SiteTree','Page','FlickrPhoto','FlickrSet',
-			'FlickrTag', 'FlickrAuthor', 'FlickrSet');
-		$this->requireDefaultRecordsFrom = $classes;
+	/*
+	Notes:
+	Searching string on number fields fails
+	http://elasticsearch-users.115913.n3.nabble.com/Error-when-searching-multiple-fields-with-different-types-td3897459.html
 
-		// add Searchable extension where appropriate
-		FlickrSet::add_extension('SilverStripe\Elastica\Searchable');
-		FlickrPhoto::add_extension('SilverStripe\Elastica\Searchable');
-		FlickrTag::add_extension('SilverStripe\Elastica\Searchable');
-		FlickrAuthor::add_extension('SilverStripe\Elastica\Searchable');
-		SearchableTestPage::add_extension('SilverStripe\Elastica\Searchable');
 
-		// load fixtures
-		parent::setUp();
+	*/
+	public function testNonTextFields() {
+		$numericFields = array(
+			'FlickrID' => 1,
+			'TakenAt' => 1,
+			'FirstViewed' => 1,
+			'Aperture' => 1,
+			'ShutterSpeed' => 1,
+			'FocalLength35mm' => 1,
+			'ISO' => 1
+		);
+
+		$this->search('New Zealand', 0, $numericFields);
+
+		//There are 10 entries in the fixtures
+		$this->search(400, 10, $numericFields);
+
 	}
+
+	/*
+	Search for stop words and assert that they are not found
+	 */
+	public function testStopWords() {
+		//Commenting::set_config_value('CommentableItem','require_moderation', true);
+		$englishIndex = new EnglishIndexSettings();
+		$stopwords = $englishIndex->getStopwords();
+		//print_r($stopwords);
+		//
+		// FIXME lenient flag needs added
+		$allFields = array(
+			'Title' => 1,
+			'FlickrID' => 1,
+			'Description' => 1,
+			'TakenAt' => 1,
+			'FirstViewed' => 1,
+			'Aperture' => 1,
+			'ShutterSpeed' => 1,
+			'FocalLength35mm' => 1,
+			'ISO' => 1
+		);
+
+		foreach ($stopwords as $stopword) {
+			//FIXME - what should happen in this case?
+			//Currently it is matching and it should not
+			//$this->search($stopword, 0);
+			$this->search($stopword, 0, array('Title' => 1));
+			$this->search($stopword, 0, array('Description' => 1));
+			$this->search($stopword, 0, array('Title' => 2, 'Description' => 1));
+
+			// this tests numeric fields also
+			$this->search($stopword, 0, $allFields);
+
+		}
+
+	}
+
+
+	public function testNonExistentField() {
+		$this->search('zealand', 0, array('Fwlubble' => 1));
+	}
+
+
+	public function testPriming() {
+		$searchableClasses = SearchableClass::get();
+		/*foreach ($searchableClasses->getIterator() as $key) {
+			echo $key->Name."\n";
+		}*/
+
+		//There are seven classes with Searchable extension added
+		$this->assertEquals(7, $searchableClasses->count());
+
+
+		$searchableFields = SearchableField::get();
+		/*foreach ($searchableFields->getIterator() as $key) {
+			echo $key->Name."\n";
+		}*/
+
+		$this->assertEquals(27, $searchableFields->count());
+
+	}
+
 
 	/**
 	 * Test searching
+	 * http://stackoverflow.com/questions/28305250/elasticsearch-customize-score-for-synonyms-stemming
 	 */
-	public function testSearching() {
-		$elasticService = Injector::inst()->create('SilverStripe\Elastica\ElasticaService');
-		//$elasticService->setLocale($this->locale);
-
-		$task = new SilverStripe\Elastica\ReindexTask($elasticService);
-		$task->run(null);
-		$flickrPhoto = $this->objFromFixture('FlickrPhoto', 'photo0001');
-
-		$query = '<b>';
+	private function search($query, $resultsExpected = 10, $fields = null) {
 		$es = new \ElasticSearcher();
 		$es->setStart(0);
-		$es->setPageLength(20);
-		$es->addFilter('IsInSiteTree', false);
-		$results = $es->search($query);
+		$es->setPageLength(100);
+		//$es->addFilter('IsInSiteTree', false);
+		$es->setClasses('FlickrPhoto');
+		$results = $es->search($query, $fields);
+		$ctr = 0;
+		/*echo "{$results->count()} items found searching for '$query'\n\n";
 		foreach ($results as $result) {
-			echo($result->Title);
+			$ctr++;
+			echo("($ctr) ".$result->Title."\n");
 			if ($result->SearchHighlightsByField->Content) {
 				foreach ($result->SearchHighlightsByField->Content as $highlight) {
 					echo("- ".$highlight->Snippet);
 				}
 			}
-
-			echo "\n\n";
 		}
-	}
+		*/
 
+		$this->assertEquals($resultsExpected, $results->count());
+
+		return $results->count();
+	}
 
 }
