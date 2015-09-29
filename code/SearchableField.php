35ff4f5a844bff9bf34727561b911603fdb46d8e
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-29 15:18:13 +0700

    MINOR: Removed obsolete method

--- code/SearchableField.php
+++ code/SearchableField.php
@@ -9,22 +9,17 @@ class SearchableField extends DataObject {
 
 	private static $has_one = array('SearchableClass' => 'SearchableClass');
 
-	private static $display_fields = array('Name','HumanReadableInSiteTree');
+	private static $display_fields = array('Name');
 
 
 	function getCMSFields() {
 		$fields = new FieldList();
-
 		$fields->push( new TabSet( "Root", $mainTab = new Tab( "Main" ) ) );
 		$mainTab->setTitle( _t( 'SiteTree.TABMAIN', "Main" ) );
 		$fields->addFieldToTab( 'Root.Main',  $nf = new TextField( 'Name', 'Name') );
 		$nf->setDisabled(true);
+
 		return $fields;
-
 	}
 
-
-	public function HumanReadableInSiteTree() {
-		return $this->IsSearched ? 'Yes':'No';
-	}
 }
