80166fe67db814ebbfd722ac191a97f0dbc23b91
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-09-30 01:01:10 +0700

    WIP: Adding more tests for utility methods, hey late at night

--- tests/ElasticSearcherUnitTest.php
+++ tests/ElasticSearcherUnitTest.php
@@ -1,5 +1,8 @@
 <?php
-class ElasticSearcherUnitTest extends SapphireTest {
+class ElasticSearcherUnitTest extends ElasticsearchBaseTest {
+
+	public static $fixture_file = 'elastica/tests/ElasticaTest.yml';
+
 
 	public function setUp() {
 		parent::setUp();
@@ -7,6 +10,42 @@ class ElasticSearcherUnitTest extends SapphireTest {
 
 	public function tearDown() {
 		parent::tearDown();
+	}
+
+
+	public function testSearchFieldMapping() {
+		$es = new ElasticSearcher();
+		$es->setClasses('SiteTree');
+		$expected = array('Title' => 'string', 'Content' => 'string');
+		$this->assertEquals($expected, $es->getSearchFieldsMappingForClasses('SiteTree'));
+	}
+
+
+	// ---- tests for field array to elasticsearch syntax
+	public function testConvertWeightedFieldsForElasticaUnaryStrings() {
+		$es = new ElasticSearcher();
+		$es->setClasses('FlickrPhoto');
+		$fields = array('Title' => 1, 'Description' => 1);
+		$expected = array('Title', 'Title.*','Description', 'Description.*');
+		$this->assertEquals($expected, $es->convertWeightedFieldsForElastica($fields));
+	}
+
+
+	public function testConvertWeightedFieldsForElasticaMultipleStrings() {
+		$es = new ElasticSearcher();
+		$es->setClasses('FlickrPhoto');
+		$fields = array('Title' => 2, 'Description' => 1);
+		$expected = array('Title^2', 'Title.*^2','Description', 'Description.*');
+		$this->assertEquals($expected, $es->convertWeightedFieldsForElastica($fields));
+	}
+
+
+	public function testConvertWeightedFieldsForElasticaTestNonString() {
+		$es = new ElasticSearcher();
+		$es->setClasses('FlickrPhoto');
+		$fields = array('Aperture' => 2, 'FocalLength35mm' => 1);
+		$expected = array('Aperture^2', 'FocalLength35mm');
+		$this->assertEquals($expected, $es->convertWeightedFieldsForElastica($fields));
 	}
 
 
