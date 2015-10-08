423ce2410c842ad74eee3bc8ac7c8d9ef802a1ac
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-08 17:39:30 +0700

    MINOR: Moved tests to QueryGeneratorTest to mirror move in classes being tested

--- tests/ElasticSearcherUnitTest.php
+++ tests/ElasticSearcherUnitTest.php
@@ -24,61 +24,6 @@ class ElasticSearcherUnitTest extends ElasticsearchBaseTest {
 	}
 
 
-	// ---- tests for field array to elasticsearch syntax
-	public function testConvertWeightedFieldsForElasticaUnaryStrings() {
-		$es = new ElasticSearcher();
-		$es->setClasses('FlickrPhoto');
-		$fields = array('Title' => 1, 'Description' => 1);
-		$expected = array('Title', 'Title.*','Description', 'Description.*');
-		$this->assertEquals($expected, $es->convertWeightedFieldsForElastica($fields));
-	}
 
 
-	public function testConvertWeightedFieldsForElasticaMultipleStrings() {
-		$es = new ElasticSearcher();
-		$es->setClasses('FlickrPhoto');
-		$fields = array('Title' => 2, 'Description' => 1);
-		$expected = array('Title^2', 'Title.*^2','Description', 'Description.*');
-		$this->assertEquals($expected, $es->convertWeightedFieldsForElastica($fields));
-	}
-
-
-	public function testConvertWeightedFieldsForElasticaTestNonString() {
-		$es = new ElasticSearcher();
-		$es->setClasses('FlickrPhoto');
-		$fields = array('Aperture' => 2, 'FocalLength35mm' => 1);
-		$expected = array('Aperture^2', 'FocalLength35mm');
-		$this->assertEquals($expected, $es->convertWeightedFieldsForElastica($fields));
-	}
-
-
-	// ---- tests for the toQuotedCSV function ----
-	public function testToQuotedCSVFromString() {
-		$expected = "'Bangkok','Nonthaburi','Saraburi','Chiang Mai'";
-		$items = 'Bangkok,Nonthaburi,Saraburi,Chiang Mai';
-		$quoted = ElasticSearcher::convertToQuotedCSV($items);
-		$this->assertEquals($expected, $quoted);
-	}
-
-	public function testToQuotedCSVFromArray() {
-		$expected = "'Bangkok','Nonthaburi','Saraburi','Chiang Mai'";
-		$items = array('Bangkok','Nonthaburi','Saraburi','Chiang Mai');
-		$quoted = ElasticSearcher::convertToQuotedCSV($items);
-		$this->assertEquals($expected, $quoted);
-	}
-
-	public function testToQuotedCSVEmptyString() {
-		$quoted = ElasticSearcher::convertToQuotedCSV('');
-		$this->assertEquals('', $quoted);
-	}
-
-	public function testToQuotedCSVEmptyArray() {
-		$quoted = ElasticSearcher::convertToQuotedCSV(array());
-		$this->assertEquals('', $quoted);
-	}
-
-	public function testToQuotedCSVNull() {
-		$quoted = ElasticSearcher::convertToQuotedCSV(null);
-		$this->assertEquals('', $quoted);
-	}
 }
