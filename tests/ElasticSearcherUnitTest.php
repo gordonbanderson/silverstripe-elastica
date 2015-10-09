bc246201c38f31d08717ba815f156eb3f3839693
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-09 21:52:06 +0700

    FIX: Namespace was incorrectly inside class definition

--- tests/ElasticSearcherUnitTest.php
+++ tests/ElasticSearcherUnitTest.php
@@ -1,11 +1,8 @@
 <?php
+use SilverStripe\Elastica\ElasticSearcher;
+
 class ElasticSearcherUnitTest extends ElasticsearchBaseTest {
-
-	use \SilverStripe\Elastica\ElasticSearcher;
-
-
 	public static $fixture_file = 'elastica/tests/ElasticaTest.yml';
-
 
 	public function setUp() {
 		parent::setUp();
@@ -15,15 +12,11 @@ class ElasticSearcherUnitTest extends ElasticsearchBaseTest {
 		parent::tearDown();
 	}
 
-
 	public function testSearchFieldMapping() {
 		$es = new ElasticSearcher();
 		$es->setClasses('SiteTree');
 		$expected = array('Title' => 'string', 'Content' => 'string');
 		$this->assertEquals($expected, $es->getSearchFieldsMappingForClasses('SiteTree'));
 	}
-
-
-
 
 }
