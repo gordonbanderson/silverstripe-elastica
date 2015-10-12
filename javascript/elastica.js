e3167aecddf4b3d97db84546beef13126bb56944
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-13 02:08:36 +0700

    WIP: Checking in and testing on a virgin setup

---
+++ javascript/elastica.js
@@ -0,0 +1,34 @@
+/*jslint white: true */
+alert('elastica js');
+(function($) {
+	$(document).ready(function() {
+		$('.facetToggle').click(function(e) {
+			alert('clicked');
+			var jel = $(e.target);
+
+			if (jel.hasClass('rotate')) {
+				jel.removeClass('rotate');
+				jel.addClass('rotateBack');
+			} else {
+				jel.removeClass('rotateBack');
+				jel.addClass('rotate');
+			}
+
+			ul = jel.parent().next();
+			console.log(ul);
+			if (ul.hasClass('facetVisible')) {
+				ul.removeClass('facetVisible');
+				ul.addClass('facetInvisible');
+				ul.slideUp(200);
+			} else {
+				ul.removeClass('facetInvisible');
+				ul.addClass('facetVisible');
+				ul.slideDown(200);
+			}
+
+		});
+	});
+
+
+
+})(jQuery);
