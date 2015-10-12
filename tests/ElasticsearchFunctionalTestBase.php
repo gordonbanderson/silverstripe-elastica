4affa5a7bf8990955f78cbbc6caeffddb33f7a54
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-12 23:38:01 +0700

    FIX: Move cache clear into setup

--- tests/ElasticsearchFunctionalTestBase.php
+++ tests/ElasticsearchFunctionalTestBase.php
@@ -16,17 +16,15 @@ class ElasticsearchFunctionalTestBase extends FunctionalTest {
 		$config->remove('Injector', 'SilverStripe\Elastica\ElasticaService');
 		$constructor = array('constructor' => array('%$Elastica\Client', 'elastica_ss_module_test'));
 		$config->update('Injector', 'SilverStripe\Elastica\ElasticaService', $constructor);
-
-		// no need to index here as it's done when fixtures are loaded during setup method
-		$cache = SS_Cache::factory('elasticsearch');
-		$cache->clean(Zend_Cache::CLEANING_MODE_ALL);
-		SS_Cache::set_cache_lifetime('elasticsearch', 3600, 1000);
-
 		parent::setUpOnce();
 	}
 
 
 	public function setUp() {
+		$cache = SS_Cache::factory('elasticsearch');
+		$cache->clean(Zend_Cache::CLEANING_MODE_ALL);
+		SS_Cache::set_cache_lifetime('elasticsearch', 3600, 1000);
+
 		// this needs to be called in order to create the list of searchable
 		// classes and fields that are available.  Simulates part of a build
 		$classes = array('SearchableTestPage','SiteTree','Page','FlickrPhoto','FlickrSet',
