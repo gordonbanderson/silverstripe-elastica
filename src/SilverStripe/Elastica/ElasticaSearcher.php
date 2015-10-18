40ec9213c3fcf74a1d86eadf3733b1dccf5a3b45
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-18 14:33:46 +0700

    FIX: Remove dead code.  Enhancement, add highlights for suggested query

--- src/SilverStripe/Elastica/ElasticaSearcher.php
+++ src/SilverStripe/Elastica/ElasticaSearcher.php
@@ -163,93 +163,6 @@ class ElasticSearcher {
 			}
 		}
 
-
-	/*
-
-		$manipulatorInstance = null;
-		if ($this->manipulator) {
-			$manipulatorInstance = \Injector::inst()->create($this->manipulator);
-			$manipulatorInstance->searcher = $this;
-			$manipulatorInstance->originalQueryString = $q;
-		}
-
-		echo "MANIPULATOR:{$this->manipulator}\n";
-
-		// update the filters, do any necessary remapping here
-		if ($this->manipulator) {
-			echo "SEARCH: UPDATING FILTERS";
-			$manipulatorInstance->updateFilters($this->filters);
-		}
-
-		$elFilters = array();
-		$rangeFilterKeys = RangedAggregation::getTitles();
-
-		foreach ($this->filters as $key => $value) {
-			echo "Checking filter $key => $value\n";
-			if (!in_array($key, $rangeFilterKeys)) {
-				$filter = new Term();
-				$filter->setTerm($key,$value);
-				$elFilters[] = $filter;
-			} else {
-				// get the selected range filter
-				$range = \RangedAggregation::getByTitle($key);
-				$filter = $range->getFilter($value);
-				$elFilters[] = $filter;
-			}
-		}
-
-
-		// if not facets selected, pass through null
-		$queryFilter = null;
-		switch (count($this->filters)) {
-			case 0:
-				// filter already null
-				break;
-			case 1:
-				$queryFilter = $elFilters[0];
-				break;
-			default:
-				$queryFilter = new BoolAnd();
-				foreach ($elFilters as $filter) {
-					$queryFilter->addFilter($filter);
-				}
-				break;
-		}
-
-		$textQuery = null;
-		$fieldsToSearch = $this->fieldsAllowedCSV;
-		if (is_array($fieldsToSearch) && sizeof($fieldsToSearch) > 0) {
-			$textQuery = new MultiMatch();
-	        $textQuery->setQuery($q);
-	        $elasticaFields = $this->convertWeightedFieldsForElastica($fieldsToSearch);
-	        //$textQuery->setFields(array('Title^4','Content','Content.*'));
-	        //$fieldsCSV = implode(',', $fieldsToSearch);
-	        $textQuery->setFields($elasticaFields);
-	        $textQuery->setType('most_fields');
-
-	        //Setting the lenient flag means that numeric fields can be searched for text values
-	        $textQuery->setParam('lenient', true);
-		} else {
-			// this will search all fields
-			$textQuery = new QueryString($q);
-
-			//Setting the lenient flag means that numeric fields can be searched for text values
-			$textQuery->setParam('lenient', true);
-		}
-
-		// the Elastica query object
-		if ($queryFilter == null) {
-			$query = new Query($textQuery);
-		} else {
-			$filtered = new Filtered(
-			  $textQuery,
-			  $queryFilter
-			);
-			$query = new Query($filtered);
-		}
-
-*/
-
 		// pagination
 
 
@@ -271,12 +184,6 @@ class ElasticSearcher {
 
 		$elasticService = \Injector::inst()->create('SilverStripe\Elastica\ElasticaService');
 		$elasticService->setLocale($this->locale);
-
-/*
-		echo "QUERY IN ES\n";
-		print_r($query);
-		die;
-*/
 
 		$resultList = new ResultList($elasticService, $query, $q);
 
@@ -301,6 +208,7 @@ class ElasticSearcher {
 		$this->aggregations = $resultList->getAggregations();
 
 		$this->SuggestedQuery = $resultList->SuggestedQuery;
+		$this->SuggestedQueryHighlighted = $resultList->SuggestedQueryHighlighted;
 
 		return $paginated;
 	}
@@ -312,6 +220,10 @@ class ElasticSearcher {
 
 	public function getSuggestedQuery() {
 		return $this->SuggestedQuery;
+	}
+
+	public function getSuggestedQueryHighlighted() {
+		return $this->SuggestedQueryHighlighted;
 	}
 
 }
