0b277499c290c5284625a688e29cb55b19d11747
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-23 10:33:33 +0700

    MINOR: Remove gibberish stop word

--- src/SilverStripe/Elastica/indexing/EnglishIndexSettings.php
+++ src/SilverStripe/Elastica/indexing/EnglishIndexSettings.php
@@ -6,7 +6,7 @@
 class EnglishIndexSettings extends BaseIndexSettings {
 
 	public function __construct() {
-		$swords = "asdfasdfffghth,that,into,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,of,on,or,";
+		$swords = "that,into,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,of,on,or,";
 		$swords .= "such,that,the,their,then,there,these,they,this,to,was,will,";
 		$swords .= "with";
 		$this->setStopWords($swords);
