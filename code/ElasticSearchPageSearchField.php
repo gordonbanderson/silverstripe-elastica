0834768a7ca88b78bb3eda929b1d8e0d3debe2ba
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-23 10:32:14 +0700

    WIP: Added separate search field option for relevance feedback

--- code/ElasticSearchPageSearchField.php
+++ code/ElasticSearchPageSearchField.php
@@ -6,17 +6,20 @@ class ElasticSearchPageSearchField extends DataObject {
 		'Weight' => 'Float', // the weighting for this field, default 1
 		'Type' => 'Varchar', // the elasticsearch indexing type
 		'Searchable' => 'Boolean', // allows the option of turning off a single field for searching
+		'SimilarSearchable' => 'Boolean', // allows field to be used in more like this queries.
 		'Active' => 'Boolean' // preserve previous edits of weighting when classes changed
 	);
 
 	private static $defaults = array(
 		'Searchable' => true,
+		//More like this works best with only a handful of fields, so default is off
+		'SearchableMoreLikeThis' => false,
 		'Weight' => 1
 	);
 
 	private static $has_one = array('ElasticSearchPage' => 'ElasticSearchPage');
 
-	private static $display_fields = array('Name','Weight','Searchable');
+	private static $display_fields = array('Name','Weight','Searchable', 'SimilarSearchable');
 
 
 	function getCMSFields() {
@@ -28,12 +31,17 @@ class ElasticSearchPageSearchField extends DataObject {
 		$nf->setDisabled(true);
 		$fields->addFieldToTab( 'Root.Main',  new NumericField( 'Weight', 'Weight') );
 		$fields->addFieldToTab( 'Root.Main',  new CheckboxField( 'Searchable', 'Search this field?') );
+		$fields->addFieldToTab( 'Root.Main',  new CheckboxField( 'SimilarSearchable', 'Use this field for similar search?') );
 		return $fields;
 	}
 
 
 	public function HumanReadableSearchable() {
 		return $this->Searchable ? 'Yes':'No';
+	}
+
+	public function HumanReadableSimilarSearchable() {
+		return $this->SimilarSearchable ? 'Yes':'No';
 	}
 
 
