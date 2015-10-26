5a7e1bc0d3caddf020a7dee1dc8ba0fd622a9ce1
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-27 00:49:24 +0700

    WIP: Moving config of language settings directly back to individual files, as URLs are not filtered correctly by _language_ names

--- src/SilverStripe/Elastica/indexing/EnglishIndexSettings.php
+++ src/SilverStripe/Elastica/indexing/EnglishIndexSettings.php
@@ -11,7 +11,49 @@ class EnglishIndexSettings extends BaseIndexSettings {
 		$swords .= "with";
 		$this->setStopWords($swords);
 		$this->setAsciiFolding(true);
-		$this->setAnalyzerType('english');
+
+		$this->addFilter('english_stop', array(
+			'type' => 'stop',
+			'stopwords' => $this->stopWords
+		));
+
+/*
+		//Add words here not to be stemmed
+		$this->addFilter('english_keywords', array(
+			'type' => 'keyword_marker',
+			'keywords' => "[]"
+		));
+*/
+
+		//Add stemmers
+		$this->addFilter('english_stemmer', array(
+			'type' => 'stemmer',
+			'keywords' => "english"
+		));
+
+		$this->addFilter('english_possessive_stemmer', array(
+			'type' => 'stemmer',
+			'keywords' => "possessive_english"
+		));
+
+		//Optional
+		$this->addFilter('english_snowball', array(
+			'type' => 'snowball',
+			'language' => 'English'
+		));
+
+
+
+
+		$englishWithURLs = array(
+			'tokenizer' => 'uax_url_email',
+			//'filter' => array('english_possessive_stemmer', 'lowercase', 'english_stop', /*'english_keywords',*/ 'english_stemmer' ),
+			'filter' => array('no_single_chars', 'english_snowball', 'lowercase', 'english_stop' ),
+			'type' => 'custom'
+		);
+
+
+		$this->addAnalyzer('stemmed', $englishWithURLs);
 	}
 
 
