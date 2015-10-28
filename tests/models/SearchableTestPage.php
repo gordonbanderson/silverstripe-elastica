a2e861fae6a3d752d77319f167750b9eb9279a13
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-28 22:27:18 +0700

    FIX: Don't redeclare Title and Content, should propogate

--- tests/models/SearchableTestPage.php
+++ tests/models/SearchableTestPage.php
@@ -5,7 +5,7 @@
  * @subpackage tests
  */
 class SearchableTestPage extends Page implements TestOnly {
-	private static $searchable_fields = array('Title', 'Content','Country','PageDate');
+	private static $searchable_fields = array('Country','PageDate');
 
 	private static $db = array(
 		'Country' => 'Varchar',
