9a1a35a1a2df766cb1f9803c7b911f83f9389ab6
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-28 22:26:51 +0700

    MINOR: Set flag for testing mode

--- tests/ElasticsearchFunctionalTestBase.php
+++ tests/ElasticsearchFunctionalTestBase.php
@@ -1,6 +1,8 @@
 <?php
 
 use SilverStripe\Elastica\ReindexTask;
+use SilverStripe\Elastica\ElasticaService;
+
 
 class ElasticsearchFunctionalTestBase extends FunctionalTest {
 	public static $ignoreFixtureFileFor = array();
@@ -16,6 +18,7 @@ class ElasticsearchFunctionalTestBase extends FunctionalTest {
 		$config->remove('Injector', 'SilverStripe\Elastica\ElasticaService');
 		$constructor = array('constructor' => array('%$Elastica\Client', 'elastica_ss_module_test'));
 		$config->update('Injector', 'SilverStripe\Elastica\ElasticaService', $constructor);
+		ElasticaService::setIsInTestMode();
 		parent::setUpOnce();
 	}
 
