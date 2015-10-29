eb82dfbf465935930afdc987ae007fc0916ae7e8
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-29 23:56:26 +0700

    WIP: Remodelling with many many to an elastic search page

--- code/SearchableField.php
+++ code/SearchableField.php
@@ -5,7 +5,13 @@ class SearchableField extends DataObject {
 		'Name' => 'Varchar', // the name of the field, e.g. Title
 		'ClazzName' => 'Varchar', // the ClassName this field belongs to
 		'Type' => 'Varchar', // the elasticsearch indexing type,
-		'Autocomplete' => 'Boolean' // Use this to check for autocomplete fields
+		'Autocomplete' => 'Boolean', // Use this to check for autocomplete fields,
+		'IsSiteTree' => 'Boolean' // Set to true if this field originates from a SiteTree object
+	);
+
+
+	private static $belongs_many_many = array(
+		'ElasticSearchPages' => 'ElasticSearchPage'
 	);
 
 	private static $has_one = array('SearchableClass' => 'SearchableClass');
