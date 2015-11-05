d7cb73536f50846b80a32f9653272b1236351f1a
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-11-06 02:36:15 +0700

    FIX: Remove debug

--- src/SilverStripe/Elastica/QueryGenerator.php
+++ src/SilverStripe/Elastica/QueryGenerator.php
@@ -204,10 +204,7 @@ class QueryGenerator {
 
 	 */
 	public function generateElasticaAutocompleteQuery() {
-
 		$field = array_keys($this->fields[0])[0];
-		print_r($field);
-
 		$data = array(
 			'size' => 10,
 			'query' => array(
