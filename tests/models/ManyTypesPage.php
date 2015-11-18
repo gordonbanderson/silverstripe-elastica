4f4e48fed7d46102138e18b445d23fc79566b839
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-11-18 16:59:41 +0700

    ENHANCEMENT: Test model with all SilverStripe data types

---
+++ tests/models/ManyTypesPage.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package elastica
+ * @subpackage tests
+ */
+class ManyTypesPage extends Page implements TestOnly {
+	private static $searchable_fields = array('BooleanField', 'BooleanField', 'BooleanField',
+		'CurrencyField', 'DateField', 'DecimalField', 'EnumField', 'HTMLTextField',
+		'HTMLVarcharField', 'IntField', 'PercentageField', 'SS_DatetimeField', 'TextField',
+		'TimeField');
+
+	private static $db = array(
+		'BooleanField' => 'Boolean',
+		'CurrencyField' => 'Currency',
+		'DateField' => 'Date',
+		'DecimalField' => 'Decimal',
+		'EnumField' => 'Enum(wibble,wobble)',
+		'HTMLTextField' => 'HTMLText',
+		'HTMLVarcharField' => 'HTMLVarchar',
+		'IntField' => 'Int',
+		'PercentageField' => 'Percentage',
+		'SS_DatetimeField' => 'SS_Datetime',
+		'TextField' => 'Text',
+		'TimeField' => 'Time'
+	);
+
+}
+
+/**
+ * @package elastica
+ * @subpackage tests
+ */
+class ManyTypesPagePage_Controller extends Controller implements TestOnly {
+}
+
