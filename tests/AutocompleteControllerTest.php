8c5e6e5726cd5a42ba58e4d66c9d99f64829a41b
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-29 14:06:18 +0700

    MINOR: Moved SiteTree test to another class

--- tests/AutocompleteControllerTest.php
+++ tests/AutocompleteControllerTest.php
@@ -20,9 +20,6 @@ class AutocompleteControllerTest extends ElasticsearchFunctionalTestBase {
 		// Delete and assert that it does not exist
 		$sql =  "SELECT ID,Name,ClazzName from SearchableField";
 		$records = DB::query($sql);
-		foreach ($records as $record) {
-			print_r($record);
-		}
 
 		$filter = array('Name' => 'Title', 'ClazzName' => 'FlickrPhotoTO');
 		$sf = SearchableField::get()->filter($filter)->first();
@@ -36,52 +33,6 @@ class AutocompleteControllerTest extends ElasticsearchFunctionalTestBase {
 		$task->run(null);
 	}
 
-
-	public function testSiteTree() {
-		$url = 'autocomplete/search?field=Title&filter=1&query=us';
-		$response = $this->get($url);
-		$this->assertEquals(200, $response->getStatusCode());
-		$body = $response->getBody();
-
-		$result = json_decode($body);
-
-		$this->assertEquals('the', $result->Query);
-		$lquery = strtolower($result->Query);
-		foreach ($result->suggestions as $suggestion) {
-			$value = $suggestion->value;
-			$value = strtolower($value);
-			$this->assertContains($lquery, $value);
-		}
-
-		// make sure there were actually some results
-		$this->assertEquals(10, sizeof($result->suggestions));
-
-		//search for different capitlisation, should produce the same result
-		$url = 'autocomplete/search?field=Title&filter=1&query=ThE';
-		$response = $this->get($url);
-		$this->assertEquals(200, $response->getStatusCode());
-		$body = $response->getBody();
-		$result2 = json_decode($body);
-		$this->assertEquals($result->suggestions, $result2->suggestions);
-
-		//append a space should produce the same result
-		$url = 'autocomplete/search?field=Title&filter=1&query=ThE%20';
-		$response = $this->get($url);
-		$this->assertEquals(200, $response->getStatusCode());
-		$body = $response->getBody();
-		$result3 = json_decode($body);
-		$this->assertEquals($result2->suggestions, $result3->suggestions);
-
-
-		// test a non existent class, for now return blanks so as to avoid extra overhead as this
-		// method is called often
-		$url = 'autocomplete/search?field=FieldThatDoesNotExist&filter=1&query=the';
-		$response = $this->get($url);
-		$this->assertEquals(200, $response->getStatusCode());
-		$body = $response->getBody();
-		$result4 = json_decode($body);
-		$this->assertEquals(0, sizeof($result4->suggestions));
-	}
 
 
 	public function testDataObject() {
