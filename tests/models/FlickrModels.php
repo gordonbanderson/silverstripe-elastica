4b436854ac4dd23bea5b95d14d12b21e8aab1a5b
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-31 00:57:22 +0700

    WIP: Incorporated ManyTypesPage

--- tests/models/FlickrModels.php
+++ tests/models/FlickrModels.php
@@ -212,3 +212,47 @@ class FlickrPhotoTOTestIndexingExtension extends Extension implements ElasticaIn
 		return $htmlFields;
 	}
 }
+
+
+/**
+ * @package elastica
+ * @subpackage tests
+ */
+class ManyTypesPage extends Page { // FIXME, TestOnly
+	private static $searchable_fields = array(
+		'BooleanField',
+		'CurrencyField',
+		'DateField',
+		'DecimalField',
+		'HTMLTextField',
+		'HTMLVarcharField',
+		'IntField',
+		'PercentageField',
+		'SS_DatetimeField',
+		'TextField',
+		'TimeField'
+	);
+
+	private static $db = array(
+		'BooleanField' => 'Boolean',
+		'CurrencyField' => 'Currency',
+		'DateField' => 'Date',
+		'DecimalField' => 'Decimal',
+		'HTMLTextField' => 'HTMLText',
+		'HTMLVarcharField' => 'HTMLVarchar',
+		'IntField' => 'Int',
+		'PercentageField' => 'Percentage',
+		'SS_DatetimeField' => 'SS_Datetime',
+		'TextField' => 'Text',
+		'TimeField' => 'Time'
+	);
+
+}
+
+/**
+ * @package elastica
+ * @subpackage tests
+ */
+class ManyTypesPage_Controller extends Controller implements TestOnly {
+}
+
