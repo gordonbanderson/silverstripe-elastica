c70e7ff2eefa27cc5aae6972470a22db445692e3
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-31 15:06:53 +0700

    FIX: Messages are now HTML encoded

--- tests/ElasticSearchPage_Validator_Test.php
+++ tests/ElasticSearchPage_Validator_Test.php
@@ -70,26 +70,26 @@ class ElasticSearchPage_Validator_Test extends ElasticsearchFunctionalTestBase {
 
 	public function testEmptyIdentifier() {
 		$this->checkForError('Identifier', '',
-			'"Identifier to allow this page to be found in form templates" is required');
+			'&quot;Identifier to allow this page to be found in form templates&quot; is required');
 			$this->checkForError('Identifier', null,
-			'"Identifier to allow this page to be found in form templates" is required');
+			'&quot;Identifier to allow this page to be found in form templates&quot; is required');
 	}
 
 
 	public function testEmptyResultsPerPage() {
 		$this->checkForError('ResultsPerPage', '',
-			"'' is not a number, only numbers can be accepted for this field");
+			"&#039;&#039; is not a number, only numbers can be accepted for this field");
 		$this->checkForError('ResultsPerPage', '',
-			"Results per page must be >=1");
+			"Results per page must be &gt;=1");
 		$this->checkForError('ResultsPerPage', 0,
-			"Results per page must be >=1");
+			"Results per page must be &gt;=1");
 	}
 
 
 	public function testEmptyClassesToSearchWithoutSiteTree() {
 		$this->ElasticSearchPage->SiteTreeOnly = false;
 		$this->checkForError('ClassesToSearch', '',
-			"Please provide at least one class to search, or select 'Site Tree Only'");
+			"Please provide at least one class to search, or select &#039;Site Tree Only&#039;");
 	}
 
 
@@ -130,6 +130,8 @@ class ElasticSearchPage_Validator_Test extends ElasticsearchFunctionalTestBase {
 
 		$errors = Session::get("FormInfo.{$form->FormName()}.errors");
 		$found = false;
+
+		error_log(print_r($errors,1));
 
 		foreach ($errors as $error) {
 			if ($error['message'] == $message && $error['fieldName'] == $fieldName) {
